576dee1e836a24940600101c769c69f964cdaf43
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2024-02-15 14:51:13 +0300

    Добавьте файлы проекта.

---
+++ Lab2RP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Tablitsa_umnozhenia_csharp
+{
+    public static void Main()
+    {
+        Console.WriteLine("Таблица умножения");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("");
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.Write(i * j + "\t");
+            }
+        }
+    }
+}
